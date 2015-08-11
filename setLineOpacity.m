ac6d2f1f76d6503713b52954b53eb45e32f45cbc
Dan O'Shea <dan@djoshea.com>
2015-08-11 14:58:08 -0700

    Removing cellvec reference

--- setLineOpacity.m
+++ setLineOpacity.m
@@ -1,7 +1,7 @@
 function setLineOpacity(s, alpha)
 % setLineOpacity(hLine, alpha)
 
-    [edge, face] = cell(numel(s), 1);
+    [edge, face] = deal(cell(numel(s), 1));
     for i = 1:numel(s)
         
         % tag it as translucent for saveFigure to
