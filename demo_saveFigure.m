d7dfaf160a2870116f87336ddd12d1d2773d1f22
Dan O'Shea <dan@djoshea.com>
2015-08-13 15:27:59 -0700

    Adding makerow defn

--- demo_saveFigure.m
+++ demo_saveFigure.m
@@ -154,3 +154,19 @@ function [ha] = shadeSimple(axh, x, y1, y2, z, varargin)
     set(axh, 'Layer', 'top')
 
 end
+
+
+function vec = makerow( vec )
+% convert vector to row vector
+
+% leave size == [1 0] alone too
+if(size(vec, 1) > size(vec,2) && isvector(vec)) && ~(size(vec, 2) == 0 && size(vec, 1) == 1)
+    vec = vec';
+end
+
+if size(vec, 1) == 0 && size(vec, 2) == 1
+    vec = vec';
+end
+
+end
+
